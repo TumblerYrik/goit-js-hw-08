3d15b4d58f53d85d81806205cba9ed92d624388f
TumblerYrik <151430788+TumblerYrik@users.noreply.github.com>
2024-05-02 13:30:48 +0300

    dz-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const saveTimeThrottled = throttle(function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000); 
+
+player.on('timeupdate', function (data) {
+  saveTimeThrottled(data.seconds);
+});
+
+window.addEventListener('load', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime !== null) {
+    player.setCurrentTime(currentTime).catch(function (error) {
+      console.error('Set current time failed', error.name, error.message);
+    });
+  }
+});
